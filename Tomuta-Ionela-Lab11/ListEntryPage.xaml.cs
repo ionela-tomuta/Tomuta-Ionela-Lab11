5ded43e9673efd01c8a8e5e2169c775fcbccfe12
IONELA-ANAMARIA TOMUȚĂ <ionela.tomuta@stud.ubbcluj.ro>
2024-12-22 13:29:01 +0200

    Add project files.

---
+++ Tomuta-Ionela-Lab11/ListEntryPage.xaml.cs
@@ -0,0 +1,38 @@
+using Tomuta_Ionela_Lab11.Models;
+using Tomuta_Ionela_Lab11.Data;
+
+namespace Tomuta_Ionela_Lab11
+{
+    public partial class ListEntryPage : ContentPage
+    {
+        public ListEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            listView.ItemsSource = await App.Database.GetShopListsAsync();
+        }
+
+        async void OnShopListAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListPage
+            {
+                BindingContext = new ShopList()
+            });
+        }
+
+        async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new ListPage
+                {
+                    BindingContext = e.SelectedItem as ShopList
+                });
+            }
+        }
+    }
+}
---
+++ Tomuta-Ionela-Lab11/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Tomuta_Ionela_Lab11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
